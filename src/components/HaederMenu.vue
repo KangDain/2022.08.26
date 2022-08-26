556c7948cd9b9f1a7af139b99794ae173665db31
KangDain <minmin10271027@gmail.com>
2022-08-26 18:36:54 +0900

    first commit

---
+++ src/components/HaederMenu.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="header-menu">
+    <router-link :to="{ name: 'home' }">Home</router-link>
+    <hr>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+#header-menu {
+  width: 100%;
+  height: 50px;
+
+  display: flex;
+  align-items: center;
+  padding: 5px 60px;
+  box-shadow: 2px 2px 7px gray;
+
+}
+
+#header-menu > a {
+  margin: 10px;
+
+  font-size: 23px;
+  font-weight: bold;
+  transition: all 0.2s;
+  position: relative;
+
+  margin-left: 50px;
+}
+#header-menu > a::before{
+  position: absolute;
+  content: "";
+  width: 0;
+  bottom: 0;
+  height: 1px;
+  transition: all 0.2s;
+}
+#header-menu > a:hover{
+  transform: scale(1.2);
+}
+#header-menu > a:hover::before{
+  width: 100%;
+  background-color: black;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,30 @@
+import { createApp } from 'vue'
+import {createRouter, createWebHistory} from 'vue-router'
+import Axios from 'axios'
+
+import App from './App.vue'
+import HomePage from './pages/HomePage.vue'
+import DataPage from './pages/DataPage.vue'
+import MemoPage from './pages/MemoPage.vue'
+import Teachhome from './pages/TeachHome.vue'
+
+const routes = [
+  { name: 'home', path: '/', component: HomePage },
+  { name: 'data', path: '/userData/:userId', component: DataPage },
+  { name: 'memo', path: '/userMemo/:userId?/:titleId?', component: MemoPage },
+  { name: 'teach_home', path: '/teach_home', component: Teachhome },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes, 
+})
+
+const app = createApp(App); //app.vue 작동 완료
+
+app.config.globalProperties.$axios = Axios;
+
+app.config.globalProperties.$usersData = [];
+
+app.use(router); //라우트 세팅
+app.mount('#app'); //html에 바인드
